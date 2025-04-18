767dfbc844c99bb6cc07300a07c6ccba8c0f784f
Jan Gajdos <jangajdos@icloud.com>
2025-04-18 17:25:17 +0200

    feat: přidání Vercel Analytics pro sledování výkonu a analýzu návštěvnosti aplikace

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import GlassNavigation from "@/components/navigation/GlassNavigation";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <GlassNavigation />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
